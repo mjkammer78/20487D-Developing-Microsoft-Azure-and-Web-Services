8fc69b4d4bbdd071531e836817cbd740e69c2ff9
Maurits Kammer <mjkammer78@hotmail.com>
2020-06-26 15:33:35 +0200

    Mod06Demos

--- AllFiles/Mod06/DemoFiles/DeployWebApp/MyWebSite/Controllers/ValuesController.cs
+++ AllFiles/Mod06/DemoFiles/DeployWebApp/MyWebSite/Controllers/ValuesController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-namespace SimpleServiceForDeploymentSlots.Controllers
+namespace MyWebSite.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
@@ -14,7 +14,7 @@ namespace SimpleServiceForDeploymentSlots.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value1", "value2", "value3" };
+            return new string[] { "value1", "value2" };
         }
 
         // GET api/values/5
